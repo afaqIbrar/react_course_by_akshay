fee517fc86ac8ecaaa1165fffdc8d9b0a0dbd842
afaqibrar <afaqibrar468@gmail.com>
2024-03-25 16:01:17 +0500

    Episode 3

--- App.js
+++ App.js
@@ -1,11 +1,90 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
-const parent = React.createElement("div", 
-    { id: "parent" }, 
-    [React.createElement('div', { id: 'child' }, 
-    [React.createElement('h1', {}, "This is react by akshay"),React.createElement('h2', {}, "I'm an h2 tag")]), React.createElement('div', { id: 'child2' }, 
-    [React.createElement('h1', {}, "I'm an h1 tag"),React.createElement('h2', {}, "I'm an h2 tag")])]
-);
-console.log(parent);
+import ReactDOM from "react-dom/client";
+import testImage from "./testImage.jpeg";
+import { FaRegUser } from "react-icons/fa";
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+// Using React.createElement
+const elem = React.createElement("div", { class: "title" },
+    [
+        React.createElement('h1', {}, "H1 Tag"),
+        React.createElement('h2', {}, "H2 Tag"),
+        React.createElement('h3', {}, "H3 Tag"),
+    ]
+)
+root.render(elem);
+
+
+
+
+
+const elemJsx = (
+    <div className="title">
+        <h1>
+            H1 by jsx
+        </h1>
+        <h2>
+            H2 by jsx
+        </h2>
+        <h3>
+            H3 by jsx
+        </h3>
+    </div>
+);
+root.render(elemJsx);
+//By making a component
+
+const FuncTitle = () => {
+    return (
+        <div className="title">
+            <h1>
+                H1 by function
+            </h1>
+            <h2>
+                H2 by function
+            </h2>
+            <h3>
+                H3 by function
+            </h3>
+        </div>
+    )
+}
+root.render(<FuncTitle />)
+
+
+
+// Component Composition
+const HeadingComponent2 = () => {
+    return (
+        <div id="container">
+            <Title />
+            <Title></Title>
+            {Title()}
+            <h3 className="heading">React Functional component</h3>
+        </div>)
+}
+
+// root.render(<HeadingComponent2 />);
+
+
+//Assignment 
+
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="logo">
+                <img className="image" src={testImage} alt="logo" />
+            </div>
+            <div>
+                <input type="text" className="search" />
+            </div>
+            <div className="user-icon">
+                <FaRegUser />
+            </div>
+        </div>
+    )
+}
+root.render(<Header />)
