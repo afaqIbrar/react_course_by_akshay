77b4d88dfaa7fd9d8f87f5f8ae9e30e492d1e192
afaqibrar <afaqibrar468@gmail.com>
2024-03-30 00:33:16 +0500

    Episode 5 assignment

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,19 +5,42 @@ import { useState } from 'react';
 const Body = () => {
   //Local State Variable - Super Powerful variable
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [searchText, setSearchText] = useState('');
+
+  const filterTopRatedRestaurants = () => {
+    const filteredListRestaurants = listOfRestaurants.filter(
+      (res) => res.data.avgRating > 4
+    );
+    setListOfRestaurants(filteredListRestaurants);
+  };
+  const filterSearchData = () => {
+    if (searchText) {
+      const filterData = listOfRestaurants.filter((res) => {
+        return res.data.name.toLowerCase().includes(searchText.toLowerCase());
+      });
+      setListOfRestaurants(filterData);
+    }
+  };
+
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredListRestaurants = listOfRestaurants.filter(
-              (res) => res.data.avgRating > 4
-            );
-            setListOfRestaurants(filteredListRestaurants);
-          }}
-        >
+        <button className="filter-btn" onClick={filterTopRatedRestaurants}>
           Top Rated Restaurants
+        </button>
+      </div>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button className="search-btn" onClick={filterSearchData}>
+          Search
         </button>
       </div>
       <div className="res-container">
---
+++ src/components/Footer.js
@@ -0,0 +1,4 @@
+const Footer = () => {
+  return <h4 className="footer">Footer</h4>;
+};
+export default Footer;
